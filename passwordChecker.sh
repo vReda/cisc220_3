c3a95f41be2342a790b8992bc4bd84f53077ea7e
vReda <15vr@queensu.ca>
2016-11-16 07:39:23 -0800

    third commit

--- passwordChecker.sh
+++ passwordChecker.sh
@@ -5,27 +5,27 @@
 echo "Please enter your password to be checked:"
 read -s $password 
 checker=0
-if [[ ${#password} >= 8 ]];
+if [ ${#password} >= 8 ];
  then
 	((checker++)) 
 fi
 echo $checker
 echo "${password}" | grep -q '[0-9]'
-if [[ $? != 0 ]]; then
+if [ $? != 0 ]; 
  then
 	((checker++))
 fi
-if [[ $password == *[@#$%&*+-=]* ]];
+if [ $password == *[@#$%&*+-=]* ];
  then
 	((checker++))
 fi
-if [[ $checker == 0 || $checker == 1 ]];
+if [ $checker == 0 || $checker == 1 ];
  then
 	echo "Weak password!"
-else if [[ $checker == 2 ]];
+else if [ $checker == 2 ];
  then
 	echo "Moderate password."
-else if [[ $checker == 3 ]];
+else if [ $checker == 3 ];
  then
 	echo "Strong password!"
 fi
