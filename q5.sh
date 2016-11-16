337fbfc5c92fc568c5159ceae053e206251c75e4
vReda <15vr@queensu.ca>
2016-11-16 07:39:49 -0800

    third commit

--- q5.sh
+++ q5.sh
@@ -3,6 +3,6 @@
 #Vanessa Reda 10194381
 
 listArray=( $(find . -type f -executable -print) )  
-for ((i=0 ; i=$(#listArray[@]) ; i++ )); do 
+for (( i=0 ; i<=$(#listArray[@]) ; i++ )); do 
 	sed -i '/bash/ a\ #Developed in CISC220 '
 done
