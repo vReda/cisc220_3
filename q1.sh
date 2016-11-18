f5f7c5bd0200aef132b66da99023f6b1ee741b1c
vReda <vanessa.reda97@gmail.com>
2016-11-18 12:34:56 -0800

    Finished product

--- q1.sh
+++ q1.sh
@@ -3,21 +3,20 @@
 #Vanessa Reda 10194381
 
 
-digit=0
-sum=0
-for (( h=1000; h<=10000; h++ )); 
+
+for (( h=1000; h<=10000; h++ )) 
 do
+sum=0
+digit=0
 i=$h
-#echo -n "Enter number:"
-#read i
-#	echo "check $i"
 	while [ $i -gt 0 ] 
 	do	
 		digit=$(( $i % 10 ))
 		i=$(( $i / 10 ))
 		sum=$(( $sum + $digit ))
 	done
-	echo "Ouput 1 $sum"
+	digit=0
+	sum2=0
 	j=$sum
 	while [ $j -gt 0 ]
 	do 
@@ -25,9 +24,8 @@ i=$h
 		j=$(( $j / 10 ))
 		sum2=$(( $sum2 + $digit ))
 	done
-	echo "output 2 $sum2"
 	if [[ $sum2 == 7 ]]; 
 	then
-		echo $sum2
+		echo $h
 	fi			 
 done
